8f4de7d1d3845f573309cb8ed890d301cbdfcf2b
Chidumebi Kennedy Ohiri <chidumebikennedyohiri@student.mitt.ca>
2022-03-24 09:21:51 -0500

    lab 2

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,10 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAnnotations.Models
 {
     public class Client
     {
-        public int Id { get; set; }
+        public int ClientNumber { get; set; }
         [StringLength(25, MinimumLength = 3, ErrorMessage = "Name must be between 3 -25 characters")]
         public string FirstName { get; set; }
 
@@ -17,6 +18,22 @@ namespace DataAnnotations.Models
         public string PhoneNumber { get; set; }
         public Room Room { get; set; }
         public Hotel Hotel { get; set; }
+        [InverseProperty("RoomNumber")]
+        public Room PreviousRoom { get; set; }
+
+        public int PreviousRoomId { get; set; }
+        [InverseProperty("RoomNumber")]
+        public Room CurrentRoom { get; set; }
+
+        public int CurrentRoomId { get; set; }
+
+        struct Credit
+        {
+            bool IsValid { get; set; }
+            [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be between 3 -20 characters")]
+            string HolderName { get; set; }
+            DateTime ExpirtDate { get; set; }
+        }
 
     }
 }
--- Models/Room.cs
+++ Models/Room.cs
@@ -1,10 +1,20 @@
-﻿namespace DataAnnotations.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAnnotations.Models
 {
     public class Room
     {
         public int RoomNumber { get; set; }
         public int Capacity { get; set; }
+        [InverseProperty("ClientNumber")]
 
+        public int CurrentClientId { get; set; }
+
+        public Client CurrentClient { get; set; }
+        [InverseProperty("ClientNumber")]
+
+        public int PreviousClientId { get; set; }
+        public Client PreviousClient { get; set; }
 
     }
     public enum Section
